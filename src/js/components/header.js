4f9b24dacefee0087e97b7b43d75cc1b8486a284
Lisenier <52318768+Lisenier@users.noreply.github.com>
2021-07-06 06:46:07 +0530

    removed erro\rs

---
+++ src/js/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CopyLink from './copy-link'
+
+export default (props) => {
+
+  const {roomName} = props
+
+  return (
+    <div id='chat-header'>
+      {roomName ? <span id='room-name'>{roomName}</span> : null}
+      {roomName ? <CopyLink /> : null}
+    </div>
+  )
+
+}
